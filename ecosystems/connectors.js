21738bd0b87bd2331bab7a8652296ac1655e5794
limjdhs <limjdhs@gmail.com>
2017-07-27 17:55:52 -0400

    resources chart

---
+++ ecosystems/connectors.js
@@ -0,0 +1,299 @@
+//food web connections
+ jsPlumb.bind("ready", function() {
+    jsPlumb.connect({ 
+        source:"carnivore-one",
+        target:"omnivore-one",
+        connector:[ "Flowchart", { midpoint:0.6} ],
+        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1]],
+        paintStyle:{ stroke:"darkred", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+    
+    jsPlumb.connect({ 
+        source:"carnivore-one",
+        target:"omnivore-two",
+        connector:[ "Flowchart", { midpoint:0.6} ],
+        anchors:[ [0.5,1,0,1,7.5,0],"Top"],
+        paintStyle:{ stroke:"darkred", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"carnivore-one",
+        target:"herbivore-one",
+        connector:[ "Flowchart", { midpoint:0.1 } ],
+        anchors:[ [0.5,1,0,1,-15,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"darkred", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+   
+    jsPlumb.connect({ 
+        source:"carnivore-one",
+        target:"herbivore-two",
+        connector:[ "Flowchart" ],
+        anchors:[ "Bottom", "Top"],
+        paintStyle:{ stroke:"darkred", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"carnivore-one",
+        target:"herbivore-three",
+        connector:[ "Flowchart", {midpoint:0.1} ],
+        anchors:[ [0.5,1,0,1,15,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"darkred", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"omnivore-two",
+        connector:[ "Flowchart"],
+        anchors:[ [1,0.5,1,0,0,5], [0,0.5,-1,0,0,5] ],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"herbivore-one",
+        connector:[ "Flowchart", { midpoint:0.55} ],
+        anchors:[ [0.5,1,0,1,-15,0], "Top"],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"herbivore-two",
+        connector:[ "Flowchart", { midpoint:0.8 } ],
+        anchors:[ "Bottom", [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"herbivore-three",
+        connector:[ "Flowchart", { midpoint:0.3 } ],
+        anchors:[ [0.5,1,0,1,15,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"producer-one",
+        connector:[ "Flowchart", { midpoint:0.075 } ],
+        anchors:[ [0.5,1,0,1,-22.5,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"producer-two",
+        connector:[ "Flowchart" ],
+        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"producer-three",
+        connector:[ "Flowchart", {midpoint:0.13} ],
+        anchors:[ [0.5,1,0,1,7.5,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-one",
+        target:"producer-four",
+        connector:[ "Flowchart", { midpoint:0.025 } ],
+        anchors:[ [0.5,1,0,1,22.5,0], [0.5,0,0,-1,-7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"omnivore-one",
+        connector:[ "Flowchart"],
+        anchors:[ [0,0.5,-1,0,0,-5], [1,0.5,1,0,0,-5] ],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"herbivore-one",
+        connector:[ "Flowchart", { midpoint:0.3 } ],
+        anchors:[ [0.5,1,0,1,-15,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"herbivore-two",
+        connector:[ "Flowchart", { midpoint:0.8 } ],
+        anchors:[ "Bottom", [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"herbivore-three",
+        connector:[ "Flowchart", { midpoint:0.55} ],
+        anchors:[ [0.5,1,0,1,15,0], "Top"],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"producer-one",
+        connector:[ "Flowchart", { midpoint:0.025 } ],
+        anchors:[ [0.5,1,0,1,-22.5,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"producer-two",
+        connector:[ "Flowchart", {midpoint:0.13} ],
+        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"producer-three",
+        connector:[ "Flowchart" ],
+        anchors:[ [0.5,1,0,1,7.5,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"omnivore-two",
+        target:"producer-four",
+        connector:[ "Flowchart", { midpoint:0.075 } ],
+        anchors:[ [0.5,1,0,1,22.5,0], [0.5,0,0,-1,7.5,0]],
+        paintStyle:{ stroke:"blue", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-one",
+        target:"producer-one",
+        connector:[ "Flowchart", { midpoint:0.125 } ],
+        anchors:[ [0.5,1,0,1,-11.25,0], [0.5,0,0,-1,-15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-one",
+        target:"producer-two",
+        connector:[ "Flowchart", { midpoint:0.375 } ],
+        anchors:[ [0.5,1,0,1,-3.75,0], [0.5,0,0,-1,-15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-one",
+        target:"producer-three",
+        connector:[ "Flowchart", { midpoint:0.625 } ],
+        anchors:[ [0.5,1,0,1,3.75,0], [0.5,0,0,-1,-15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-one",
+        target:"producer-four",
+        connector:[ "Flowchart", { midpoint:0.125 } ],
+        anchors:[ [0.5,1,0,1,11.25,0], [0.5,0,0,-1,-15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-two",
+        target:"producer-one",
+        connector:[ "Flowchart", { midpoint:0.375 } ],
+        anchors:[ [0.5,1,0,1,-11.25,0], "Top"],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-two",
+        target:"producer-two",
+        connector:[ "Flowchart", { midpoint:0.625 } ],
+        anchors:[ [0.5,1,0,1,-3.75,0], "Top"],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-two",
+        target:"producer-three",
+        connector:[ "Flowchart", { midpoint:0.625 } ],
+        anchors:[ [0.5,1,0,1,3.75,0], "Top"],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-two",
+        target:"producer-four",
+        connector:[ "Flowchart", { midpoint:0.375 } ],
+        anchors:[ [0.5,1,0,1,11.25,0], "Top"],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-three",
+        target:"producer-one",
+        connector:[ "Flowchart", { midpoint:0.125 } ],
+        anchors:[ [0.5,1,0,1,11.25,0], [0.5,0,0,-1,15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-three",
+        target:"producer-two",
+        connector:[ "Flowchart", { midpoint:0.375 } ],
+        anchors:[ [0.5,1,0,1,3.75,0], [0.5,0,0,-1,15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-three",
+        target:"producer-three",
+        connector:[ "Flowchart", { midpoint:0.625 } ],
+        anchors:[ [0.5,1,0,1,-3.75,0], [0.5,0,0,-1,15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+
+    jsPlumb.connect({ 
+        source:"herbivore-three",
+        target:"producer-four",
+        connector:[ "Flowchart", { midpoint:0.125 } ],
+        anchors:[ [0.5,1,0,1,-11.25,0], [0.5,0,0,-1,15,0]],
+        paintStyle:{ stroke:"green", strokeWidth:2 },
+        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
+    });
+});
--- ecosystems/script.js
+++ ecosystems/script.js
@@ -1,315 +1,13 @@
-//food web connections
- jsPlumb.bind("ready", function() {
-    jsPlumb.connect({ 
-        source:"carnivore-one",
-        target:"omnivore-one",
-        connector:[ "Flowchart", { midpoint:0.6} ],
-        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1]],
-        paintStyle:{ stroke:"darkred", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-    
-    jsPlumb.connect({ 
-        source:"carnivore-one",
-        target:"omnivore-two",
-        connector:[ "Flowchart", { midpoint:0.6} ],
-        anchors:[ [0.5,1,0,1,7.5,0],"Top"],
-        paintStyle:{ stroke:"darkred", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"carnivore-one",
-        target:"herbivore-one",
-        connector:[ "Flowchart", { midpoint:0.1 } ],
-        anchors:[ [0.5,1,0,1,-15,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"darkred", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-   
-    jsPlumb.connect({ 
-        source:"carnivore-one",
-        target:"herbivore-two",
-        connector:[ "Flowchart" ],
-        anchors:[ "Bottom", "Top"],
-        paintStyle:{ stroke:"darkred", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"carnivore-one",
-        target:"herbivore-three",
-        connector:[ "Flowchart", {midpoint:0.1} ],
-        anchors:[ [0.5,1,0,1,15,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"darkred", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"omnivore-two",
-        connector:[ "Flowchart"],
-        anchors:[ [1,0.5,1,0,0,5], [0,0.5,-1,0,0,5] ],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"herbivore-one",
-        connector:[ "Flowchart", { midpoint:0.55} ],
-        anchors:[ [0.5,1,0,1,-15,0], "Top"],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"herbivore-two",
-        connector:[ "Flowchart", { midpoint:0.8 } ],
-        anchors:[ "Bottom", [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"herbivore-three",
-        connector:[ "Flowchart", { midpoint:0.3 } ],
-        anchors:[ [0.5,1,0,1,15,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"producer-one",
-        connector:[ "Flowchart", { midpoint:0.075 } ],
-        anchors:[ [0.5,1,0,1,-22.5,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"producer-two",
-        connector:[ "Flowchart" ],
-        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"producer-three",
-        connector:[ "Flowchart", {midpoint:0.13} ],
-        anchors:[ [0.5,1,0,1,7.5,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-one",
-        target:"producer-four",
-        connector:[ "Flowchart", { midpoint:0.025 } ],
-        anchors:[ [0.5,1,0,1,22.5,0], [0.5,0,0,-1,-7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"omnivore-one",
-        connector:[ "Flowchart"],
-        anchors:[ [0,0.5,-1,0,0,-5], [1,0.5,1,0,0,-5] ],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"herbivore-one",
-        connector:[ "Flowchart", { midpoint:0.3 } ],
-        anchors:[ [0.5,1,0,1,-15,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"herbivore-two",
-        connector:[ "Flowchart", { midpoint:0.8 } ],
-        anchors:[ "Bottom", [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"herbivore-three",
-        connector:[ "Flowchart", { midpoint:0.55} ],
-        anchors:[ [0.5,1,0,1,15,0], "Top"],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"producer-one",
-        connector:[ "Flowchart", { midpoint:0.025 } ],
-        anchors:[ [0.5,1,0,1,-22.5,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"producer-two",
-        connector:[ "Flowchart", {midpoint:0.13} ],
-        anchors:[ [0.5,1,0,1,-7.5,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"producer-three",
-        connector:[ "Flowchart" ],
-        anchors:[ [0.5,1,0,1,7.5,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"omnivore-two",
-        target:"producer-four",
-        connector:[ "Flowchart", { midpoint:0.075 } ],
-        anchors:[ [0.5,1,0,1,22.5,0], [0.5,0,0,-1,7.5,0]],
-        paintStyle:{ stroke:"blue", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-one",
-        target:"producer-one",
-        connector:[ "Flowchart", { midpoint:0.125 } ],
-        anchors:[ [0.5,1,0,1,-11.25,0], [0.5,0,0,-1,-15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-one",
-        target:"producer-two",
-        connector:[ "Flowchart", { midpoint:0.375 } ],
-        anchors:[ [0.5,1,0,1,-3.75,0], [0.5,0,0,-1,-15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-one",
-        target:"producer-three",
-        connector:[ "Flowchart", { midpoint:0.625 } ],
-        anchors:[ [0.5,1,0,1,3.75,0], [0.5,0,0,-1,-15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-one",
-        target:"producer-four",
-        connector:[ "Flowchart", { midpoint:0.125 } ],
-        anchors:[ [0.5,1,0,1,11.25,0], [0.5,0,0,-1,-15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-two",
-        target:"producer-one",
-        connector:[ "Flowchart", { midpoint:0.375 } ],
-        anchors:[ [0.5,1,0,1,-11.25,0], "Top"],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-two",
-        target:"producer-two",
-        connector:[ "Flowchart", { midpoint:0.625 } ],
-        anchors:[ [0.5,1,0,1,-3.75,0], "Top"],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-two",
-        target:"producer-three",
-        connector:[ "Flowchart", { midpoint:0.625 } ],
-        anchors:[ [0.5,1,0,1,3.75,0], "Top"],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-two",
-        target:"producer-four",
-        connector:[ "Flowchart", { midpoint:0.375 } ],
-        anchors:[ [0.5,1,0,1,11.25,0], "Top"],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-three",
-        target:"producer-one",
-        connector:[ "Flowchart", { midpoint:0.125 } ],
-        anchors:[ [0.5,1,0,1,11.25,0], [0.5,0,0,-1,15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-three",
-        target:"producer-two",
-        connector:[ "Flowchart", { midpoint:0.375 } ],
-        anchors:[ [0.5,1,0,1,3.75,0], [0.5,0,0,-1,15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-three",
-        target:"producer-three",
-        connector:[ "Flowchart", { midpoint:0.625 } ],
-        anchors:[ [0.5,1,0,1,-3.75,0], [0.5,0,0,-1,15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-
-    jsPlumb.connect({ 
-        source:"herbivore-three",
-        target:"producer-four",
-        connector:[ "Flowchart", { midpoint:0.125 } ],
-        anchors:[ [0.5,1,0,1,-11.25,0], [0.5,0,0,-1,15,0]],
-        paintStyle:{ stroke:"green", strokeWidth:2 },
-        endpoint:[ "Rectangle", { width:5, height:5 }, "Rectangle", { width:5, height:5 } ]
-    });
-});
 
 const activeStatus = {
     "carnivore-one": true,
     //ecvry1
 }
 
-console.log('asdf')
-
 $('.organism').click(function() {
     activeStatus[this.id] = !activeStatus[this.id]
     $(this).toggleClass('active')
 })
-console.log('asdf')
 
 function toggleStatus(organism) {
     const newData = {};
@@ -323,34 +21,77 @@ function toggleStatus(organism) {
     }
 }
 
+// global variables
+var day = 0;
 
-//resources chart
-new Chart(document.getElementById("resources-chart"), {
-  type: 'line',
-  data: {
-    labels: [0,10,20,30,40,50,60,70,80,90,100],
-    datasets: [{ 
-        data: [100,100,90,80,90,70,80,90,100,100,90],
-        label: "Habitat",
-        borderColor: "green",
-        fill: false
-      }, { 
-        data: [100,90,80,70,60,50,40,50,60,70,80],
-        label: "Water",
-        borderColor: "blue",
-        fill: false
-      }, { 
-        data: [100,50,60,70,80,90,100,90,100,90,100],
-        label: "Air Quality",
-        borderColor: "purple",
-        fill: false
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: false,
+// adjusts resource values based on sliders
+$('#step').click(function() {
+    if (day != 110) {
+        document.querySelector("#counter").innerHTML = day;
+
+        trace1.y[day/10] = document.querySelector("#habitatOutput").innerHTML;
+        trace2.y[day/10] = document.querySelector("#waterOutput").innerHTML;
+        trace3.y[day/10] = document.querySelector("#airOutput").innerHTML;
+        
+        Plotly.newPlot('resources-chart', data, layout);
+
+        day += 10;
     }
-  }
-});
+})
+
+// resources data
+var trace1 = {
+    x: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+    y: [100],
+    mode: 'lines+markers',
+    name: 'Habitat'
+};
+
+var trace2 = {
+    x: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+    y: [100],
+    mode: 'lines+markers',
+    name: 'Water'
+};
+
+var trace3 = {
+    x: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+    y: [100],
+    mode: 'lines+markers',
+    name: 'Air Quality'
+};
+
+// which resources to chart
+var data = [trace1, trace2, trace3];
+
+// layout of graph
+var layout = {
+    width:500,
+    height:300,
+    margin: {
+        t: 20,
+        r: 20,
+        l: 50,
+        b: 45,
+    },
+    xaxis: {
+        title: 'Days',
+        dtick: 10,
+        range: [0, 100]
+    },
+    yaxis: {
+        title: 'Amount of Resource',
+        range: [0, 102]
+    },
+    showlegend: true,
+    legend: {
+        "orientation": "h",
+        xanchor: "center",
+        y: 1.2,
+        x: 0.5
+    }
+};
+
+//draws initial graph
+Plotly.newPlot('resources-chart', data, layout);
 
