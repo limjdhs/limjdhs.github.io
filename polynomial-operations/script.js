a23d66eb33583cb758861cb2e3dd1fc8fad84f56
limjdhs <limjdhs@gmail.com>
2017-08-09 17:04:59 -0400

    boxes 1-4 and colors

--- polynomial-operations/script.js
+++ polynomial-operations/script.js
@@ -1,42 +1,62 @@
 var scale = 100;
 
-$('#compute').click(function() {
-	var m1 = document.getElementById("m1").value
-	var b1 = document.getElementById("b1").value
-	var m2 = document.getElementById("m2").value
-	var b2 = document.getElementById("b2").value
-	// console.log(m1 + " " + b1 + " " + m2 + " " + b2) 
+function addBoxes(box, rows, columns, label) {
+	var string = ""
+	for (i=0; i<rows; i++) {
+		string += "<div class='sim-row' style='height:"+100/rows+"%'>"
+		for (j=0; j<columns; j++) {
+			string +="<div class='sim-column' style='width:"+100/columns+"%'>"+label+"</div>"
+		}
+		string += "</div>"
+	}
+	document.getElementById(box).innerHTML = string;
+}
 
-	$("#m1-label").html(`${m1}x`);
-	$("#b1-label").html(`${b1}`);
-	$("#m2-label").html(`${m2}x`);
-	$("#b2-label").html(`${b2}`);
+// $('#compute').click(function() {
+$(".input").each(function() {
+	$(this).on('input', function() {
+		console.log(this);
+		var m1 = document.getElementById("m1").value
+		var b1 = document.getElementById("b1").value
+		var m2 = document.getElementById("m2").value
+		var b2 = document.getElementById("b2").value
 
-	$('#m2-label').css({
-		'width':`${m2 * scale}px`
-	})
+		$("#m1-label").html(`${m1}x`);
+		$("#b1-label").html(`${b1}`);
+		$("#m2-label").html(`${m2}x`);
+		$("#b2-label").html(`${b2}`);
 
-	$('#b2-label').css({
-		'width':`${b2 * 0.3 * scale}px`
-	})
+		addBoxes("box-one", m1, m2, "<span>x<sup>2</sup></span>");
+		addBoxes("box-two", m1, b2, "x");
+		addBoxes("box-three", b1, m2, "x");
+		addBoxes("box-four", b1, b2, "1");
 
-	$('#box-one').css({
-		'width':`${m2 * scale}px`,
-		'height':`${m1 * scale}px`
-	})
+		$('#m2-label').css({
+			'width':`${m2 * scale}px`
+		})
 
-	$('#box-two').css({
-		'width':`${b2 * 0.3 * scale}px`,
-		'height':`${m1 * scale}px`
-	})
+		$('#b2-label').css({
+			'width':`${b2 * 0.3 * scale}px`
+		})
 
-	$('#box-three').css({
-		'width':`${m2 * scale}px`,
-		'height':`${b1 * 0.3 * scale}px`
-	})
+		$('#box-one').css({
+			'width':`${m2 * scale}px`,
+			'height':`${m1 * scale}px`
+		})
 
-	$('#box-four').css({
-		'width':`${b2 * 0.3 * scale}px`,
-		'height':`${b1 * 0.3 * scale}px`
-	})
+		$('#box-two').css({
+			'width':`${b2 * 0.3 * scale}px`,
+			'height':`${m1 * scale}px`
+		})
+
+		$('#box-three').css({
+			'width':`${m2 * scale}px`,
+			'height':`${b1 * 0.3 * scale}px`
+		})
+
+		$('#box-four').css({
+			'width':`${b2 * 0.3 * scale}px`,
+			'height':`${b1 * 0.3 * scale}px`
+		})
+	});
 })
